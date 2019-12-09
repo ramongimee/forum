090e035b94a1827f27d390533291a1441b72d4d8
Ramon Gimenes <ramongimee@hotmail.com>
2019-12-09 00:57:37 -0300

    Proteção com Spring Security

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$.zZd.ukSIF77RDaL2U4yJexf.veI0vGGLUMe54qftXGy0W3mgo8jO');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
